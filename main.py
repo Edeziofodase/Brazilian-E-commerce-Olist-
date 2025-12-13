79b8702ea5a02abdcb377accd4c802e90d1b2c5a
Edezio Barros <100710231+Edeziofodase@users.noreply.github.com>
2025-12-13 00:38:31 -0300

    Add data processing for Brazilian e-commerce dataset

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 import streamlit as st
+import pandas as pd
+import math as m
+import numpy as np
+import seaborn as sns
+import os
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+import folium
+from folium.plugins import MarkerCluster, FastMarkerCluster
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
+
+print("Path to dataset files:", path)
 st.title('Análise Geral')
+
+
+print("Arquivos no dataset:")
+for file in os.listdir(path):
+    print(f" - {file}")
+
+dadosv = pd.read_csv(os.path.join(path, 'olist_sellers_dataset.csv'))
+dadosv.head()
+
+dadosc = pd.read_csv(os.path.join(path, 'olist_customers_dataset.csv'))
+dadosc.head()
+
+dadosg = pd.read_csv(os.path.join(path, 'olist_geolocation_dataset.csv'))
+dadosg.head()
+
+dadospag = pd.read_csv(os.path.join(path, 'olist_order_payments_dataset.csv'))
+dadospag.head()
