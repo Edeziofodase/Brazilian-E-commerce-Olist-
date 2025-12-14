54005946451c3f6ffcd61f5cd21c03e35fd7c6cf
Edezio Barros <100710231+Edeziofodase@users.noreply.github.com>
2025-12-14 20:07:25 -0300

    Remove shape verification print statements
    Removed print statements for shape verification.

--- main.py
+++ main.py
@@ -32,11 +32,6 @@ dataset_path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
 customers_df = pd.read_csv(f"{dataset_path}/olist_customers_dataset.csv")
 orders_df = pd.read_csv(f"{dataset_path}/olist_orders_dataset.csv")
 
-# PASSO 3 - Verifique se funcionou
-print("Funcionou! Shapes:")
-print(f"Customers: {customers_df.shape}")
-print(f"Orders: {orders_df.shape}")
-
 # PASSO 4 - Se funcionar, carregue os outros
 geolocation_df = pd.read_csv(f"{dataset_path}/olist_geolocation_dataset.csv")
 order_items_df = pd.read_csv(f"{dataset_path}/olist_order_items_dataset.csv")
