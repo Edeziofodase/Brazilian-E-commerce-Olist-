c586d7bec7af2c189b86c81603bd9f13c8a9cd4c
Edezio Barros <100710231+Edeziofodase@users.noreply.github.com>
2025-12-14 17:31:00 -0300

    Integrate Kaggle dataset download and loading
    Added functionality to download and load datasets from Kaggle.

--- main.py
+++ main.py
@@ -15,6 +15,29 @@ def load_data():
 
 df = load_data()
 
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
+
+print("Path to dataset files:", path)
+
+print("Arquivos no dataset:")
+for file in os.listdir(path):
+    print(f" - {file}")
+
+dadosv = pd.read_csv(os.path.join(path, 'olist_sellers_dataset.csv'))
+dadosv.head()
+
+dadosc = pd.read_csv(os.path.join(path, 'olist_customers_dataset.csv'))
+dadosc.head()
+
+dadosg = pd.read_csv(os.path.join(path, 'olist_geolocation_dataset.csv'))
+dadosg.head()
+
+dadospag = pd.read_csv(os.path.join(path, 'olist_order_payments_dataset.csv'))
+dadospag.head()
+
 mapa = folium.Map(location=[dadosg['geolocation_lat'].mean(), dadosg['geolocation_lng'].mean()], zoom_start=5, tiles='CartoDB dark_matter');
 
 data = list(zip(dadosg['geolocation_lat'], dadosg['geolocation_lng']))
